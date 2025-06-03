5b4dd1286d215fc2a5dd7dc360f5dcbf63113c52
NaveenKumarSena <naveenkumar@sena.services>
2025-06-03 12:21:44 +0530

    frontend changes pushed

--- blnd-frontend/src/site-ui/sections/about-us/index.tsx
+++ blnd-frontend/src/site-ui/sections/about-us/index.tsx
@@ -6,47 +6,48 @@ import React from "react";
 
 const AboutUs = ({ data }: { data: HomeDataAboutUs }) => {
   return (
-    <div >
-      <SectionSpacingLayout id="about-us"   className="bg-pink-50 rounded-2xl p-4">
+    <div>
+      <SectionSpacingLayout
+        id="about-us"
+        className="bg-pink-50 rounded-2xl p-4"
+      >
         <HeadingLayout heading="About Us" />
         <h2 className="text-3xl text-brand-secondary font-medium text-center">
           {data.title}
         </h2>
 
-        <div className="space-y-4 mt-10">
+        <div className="space-y-10 mt-10">
           {data?.about_us_section.map((sec, i) => (
-            <React.Fragment key={i}>
-              <div
-                className={`flex flex-col rounded-2xl items-start lg:p-6 ${
-                  i % 2 === 0 ? "md:flex-row " : "md:flex-row-reverse "
-                } space-x-4`}
-              >
-                <div className="md:w-1/3 w-full flex-col flex items-center justify-center">
-                  <h2
-                    className={`text-5xl text-center ${
-                      i % 2 === 0 ? "md:text-left" : "md:text-right"
-                    } font-semibold text-red-600`}
-                  >
-                    {sec.section_title}
-                  </h2>
-                  <div>
+            <div
+              key={i}
+              className={`flex flex-col md:flex-row ${
+                i % 2 !== 0 ? "md:flex-row-reverse" : ""
+              } gap-6 items-stretch`}
+            >
+              <div className="w-full flex flex-col">
+                <h2
+                  className={`text-5xl font-semibold text-red-600 mb-4 ${
+                    i % 2 === 0 ? "text-left" : "text-right"
+                  }`}
+                >
+                  {sec.section_title}
+                </h2>
+                <div className="w-full">
+                  <div className="text-justify text-2xl leading-9 text-brand-secondary">
                     <img
                       src={base_api_url + sec.about_us_media.url}
-                      className="md:h-[500px] md:w-[500px] w-[200px] h-[200px] object-contain"
+                      alt="About Us Media"
+                      className="w-full  md:w-1/3 mb-4 mr-0 sm:mr-0 md:mr-6 rounded-lg object-cover float-none md:float-left"
+                    />
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: sec.section_description,
+                      }}
                     />
                   </div>
                 </div>
-                <div className="md:w-2/3 w-full" key={i}>
-                  <div
-                    className="text-2xl lg:text-3xl lg:leading-12 lg:mt-0 mt-4 text-brand-secondary font-normal text-center md:text-justify"
-                    dangerouslySetInnerHTML={{
-                      __html: sec.section_description,
-                    }}
-                  />
-                </div>
               </div>
-              
-            </React.Fragment>
+            </div>
           ))}
         </div>
       </SectionSpacingLayout>
